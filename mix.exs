529cb2c50787c47bb41fe222845763c232b86f8b
Sven Bohm <bohms@msu.edu>
2016-02-03 21:01:20 -0500

    added credo for linting

--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule Pakker.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.2", only: [:dev, :test]}
+    ]
   end
 end
