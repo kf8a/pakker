fc4c8b5c0f20053ac69fc3412c23fa4e6941fc99
Sven Bohm <bohms@msu.edu>
2016-03-06 18:23:39 -0500

    signature compiles

--- test/signature_test.exs
+++ test/signature_test.exs
@@ -30,54 +30,46 @@ defmodule Pakker.Signature do
   def sig(_message) do
   end
 
-  # def calcSig(message, seed \\ 0xAAAA) do
-  #   ret = seed
-  #   csi_alg(message, seed)
-  # end
-
-  # def csi_alg(message, seed, n) when n <= 1 do
-  #   seed
-  # end
-
-  # def csi_alg(message, seed, n) do
-  #   old_seed = seed
-  #   seed 
-  #   |> shift_and_add_1ff
-  #   |> increment_seed 
-  #   |> compute_seed(message, old_seed)
-  # end
-
-  # def increment_seed(seed) when seed >= 0x100 do
-  #   seed + 1
-  # end
-
-  # def increment_seed(seed) do
-  #   seed
-  # end
-
-  # def shift_and_add_1ff(seed) do
-  #   band(seed <<< 1), 0x01ff)
-  # end
-
-  def compute_seed(message, old_seed) do
+  def calc_sig(message, seed \\ 0xAAAA) do
+    Enum.map(message, fn x -> csi_alg(x,seed) end)
   end
 
-  def compute_seed_with_char(seed, c, j) do
-    bor(band(seed + (j >>> 8) + c, 0xFF), band(j <<< 8, 0xffff))
+  def csi_alg(byte, seed) do
+    old_seed = seed
+    seed 
+    |> shift_and_add_1ff
+    |> increment_seed 
+    |> compute_seed(byte, old_seed)
+  end
+
+  def increment_seed(seed) when seed >= 0x100 do
+    band(seed + 1, 0xffff)
+  end
+
+  def increment_seed(seed) do
+    seed
+  end
+
+  def shift_and_add_1ff(seed) do
+    band(band(seed <<< 1, 0x01ff), 0xffff)
+  end
+
+  def compute_seed(seed, c, j) do
+    bor(band(seed + (j >>> 8) + c, 0xff), band(j <<< 8, 0xffff))
   end
 end
 
-defmodule SignatureTest do
+defmodule Signaturetest do
   use ExUnit.Case
 
   test 'compute seed' do
-    assert 0xaaf5 == Pakker.Signature.compute_seed_with_char(0xaaa , 65, 0xaaa)
+    assert 0xaaf5 == Pakker.Signature.compute_seed(0xaaa , 65, 0xaaa)
   end
 
   # test 'compute signature bytes' do
   #   message = << 0x90, 0x01, 0x0f, 0x71 >>
   #   correct_signature = <<0x71, 0xd2 >>
-  #   signature = Pakker.Signature.sig(message)
+  #   signature = Pakker.Signature.calc_sig(message)
   #   assert signature == correct_signature
   # end
 end
